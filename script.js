dd3f283d17f79e0c02fd8e71f577372bdbab2eb9
kulonik <annakhmv@gmail.com>
2024-06-23 13:15:19 +0500

    create grid and divs inside grid in js file

---
+++ script.js
@@ -0,0 +1,16 @@
+const container = document.getElementById('container');
+
+function makeGrid(num) {
+  container.style.gridTemplateColumns = `repeat(${num}, minmax(0px, 1fr))`;
+}
+makeGrid(32);
+
+function createDivs(num) {
+  for (let i = 1; i <= num * num; i++) {
+    let div = document.createElement('div');
+    div.classList.add('grid');
+    div.style.border = "black solid 1px";
+    container.appendChild(div);
+  }
+}
+createDivs(32);
