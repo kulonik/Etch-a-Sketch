b0e43e6362f75504f6c588ca922bb8285a11affb
kulonik <annakhmv@gmail.com>
2024-06-23 22:19:07 +0500

    Add functionality to create grids button

--- script.js
+++ script.js
@@ -5,23 +5,29 @@ const gridsNumber = document.getElementById('grids-number');
 function makeGrid(num) {
   container.style.gridTemplateColumns = `repeat(${num}, minmax(0px, 1fr))`;
 }
-makeGrid(32);
+makeGrid(16);
 
 function createDivs(num) {
   for (let i = 1; i <= num * num; i++) {
     let div = document.createElement('div');
     div.classList.add('grid');
-    div.style.border = "black solid 1px";
+    div.style.border = "#322f2f solid 0.5px";
     container.appendChild(div);
 div.addEventListener('mouseover', ()=> div.style.background = '#000');
   }
 }
-createDivs(32);
+createDivs(16);
 
-function resetDivs() {
+function resetDivs(num) {
   container.innerHTML = '';
-  createDivs(32);
+  makeGrid(16);
+  createDivs(16);
   }
 
-newGrids.addEventListener('click', () => resetDivs());
+function changeGrid(num) {
+  makeGrid(num);
+  createDivs(num);
+}
 
+newGrids.addEventListener('click', () => resetDivs());
+gridsNumber.addEventListener('click', () => changeGrid(num = prompt("Type number of grids", '')))
